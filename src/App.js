7d3521dcea43a9e21e8b25a1f258819674c8164c
Eman Domoos <emandomoos@outlook.com>
2024-04-22 15:33:09 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
+import { useState } from 'react';
+import CartList from './components/CartList';
+
+function App() {
+
+  const [product, setProduct] = useState([
+    {
+      url: 'https://static.nike.com/a/images/t_default/80b8277d-ede8-4885-9c37-2fe1cdf341aa/air-max-90-shoes-K0mczj.png',
+      name: 'Nike Air Max 90',
+      category: 'Shoes',
+      seller: 'Nike Official Store',
+      price: 3499
+    },
+    {
+      url: 'https://www.capital.com.ph/cdn/shop/products/GX9140-140_grande.jpg?v=1664769628',
+      name: 'Adidas Ultraboost',
+      category: 'Shoes',
+      seller: 'Adidas Online',
+      price: 2999
+    },
+    {
+      url: 'https://www.capital.com.ph/cdn/shop/products/373308-01.jpg?v=1663221741',
+      name: 'Puma RS-X3',
+      category: 'Shoes',
+      seller: 'Puma Store',
+      price: 2499
+    },
+    {
+      url: 'https://i.ebayimg.com/images/g/JmMAAOSw~otWdL5u/s-l1200.jpg',
+      name: 'Vans Old Skool',
+      category: 'Shoes',
+      seller: 'Vans Official',
+      price: 1999
+    },
+  ])
+
+  const [cart, setCart] = useState([])
+  const [showCart, setShowCart] = useState(false)
+
+  const addToCart = (data) => {
+    setCart([...cart, { ...data, quantity: 1 }])
+  }
+
+  const handleShow = (value) => {
+    setShowCart(value)
+  }
+
+  return (
+    <div>
+      <Header count={cart.length}
+        handleShow={handleShow} ></Header>
+
+      {
+        showCart ?
+          <CartList cart={cart} ></CartList> :
+          <ProductList product={product} addToCart={addToCart} ></ProductList>
+      }
+
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../App.css';
+
+function Header(props) {
+  return (
+    <div className='flex shopping-card'>
+      <div onClick={() => props.handleShow(false)}>Home</div>
+      <div>
+        <img src="https://w7.pngwing.com/pngs/156/414/png-transparent-shopping-cart-drawing-computer-icons-empty-cart-angle-text-rectangle-thumbnail.png" alt="Cart" onClick={() => props.handleShow(true)} style={{ 
+          background: 'transparent', mixBlendMode: 'multiply', width: '30px' }} />
+        <sup>{props.count}</sup>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
